f40233f12260c6c123413881c54cf4738d29cc64
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2012-06-26 02:24:17 -0300

    Alteração no script de carregamento de conteúdo.

--- webroot/js/alunos_index.js
+++ webroot/js/alunos_index.js
@@ -13,6 +13,9 @@ AjaxTabHandler.prototype.handleResponse = function(data, tabId) {
 		case "faltas-tab":
 			return this.handleResponseFaltas(data, tabId);
 			break;
+		case "avaliacoes_faltas-tab":
+			return this.handleResponseAvaliacoesFaltas(data, tabId);
+			break;
 		case "conteudo_programatico-tab":
 			break;
 	}
@@ -26,14 +29,12 @@ AjaxTabHandler.prototype.handleResponseCursos = function(data, tabId) {
 }
 
 AjaxTabHandler.prototype.handleResponseNotas = function(data, tabId) {
-	$("table tbody", tabId).empty();
+	$("#" + tabId + " tbody").empty();
 	$(data).each(function(i, e) {
 		tr = $("<tr>");
-		tr.append($("<td>").text(e[0].curso));
-		tr.append($("<td>").text(e[0].nome_elemento_curricular));
 		tr.append($("<td>").text(e[0].desc_ava));
 		tr.append($("<td>").text(e[0].nota));
-		$("table tbody", tabId).append(tr);
+		$("#" + tabId + " tbody").append(tr);
 	});
 }
 
@@ -42,43 +43,53 @@ AjaxTabHandler.prototype.handleResponseFaltas = function(data, tabId) {
 	qtdPresencas = 0;
 	qtdFaltas = 0;
 
-//	total = $("table tbody tr.total", tabId);
-	$("table tbody", tabId).empty();
-//	$("table tbody", tabId).append(total);
+	$("#" + tabId + " tbody").empty();
 	$(data).each(function(i, e) {
 		qtdAulas++;
 		e[0].presenca ? qtdPresencas++ : qtdFaltas++;
 		tr = $("<tr>");
 		dataAula = e[0].data_aula.split("-");
-		tr.prepend($("<td>").text((e[0].presenca ? "P" : "F")).attr("colspan", 2));
+		tr.prepend($("<td>").text((e[0].presenca ? "P" : "F")));
 		tr.prepend($("<td>").text(dataAula[2] + "/" + dataAula[1] + "/" + dataAula[0]));
-		tr.prepend($("<td>").text(qtdAulas));
-		$("table tbody", tabId).append(tr);
+		$("#" + tabId + " tbody").append(tr);
 	});
 
 	tr = $("<tr>");
 	tr.append($("<td>").html("<p><b><i class=\"icon-ok\"></i> Presenças: </b>" + qtdPresencas + "<br /><b><i class=\"icon-remove\"></i> Faltas: </b>" + qtdFaltas + "</p>").attr("colspan", 3));
-	$("table tbody", tabId).append(tr);
+	$("#" + tabId + " tbody").append(tr);
 }
 
-$(document).ready(function() {
-	$(".tab").click(function(e) {
-		var ajaxTabHandler = new AjaxTabHandler($(this).attr("id"));
-
-		tab = $(this);
-		tabId = $("a", tab).attr("href");
-		$.ajax({
-			url: $(this).attr("href"),
-			dataType: "json",
-			success: function(data, textStatus, jqXHR) {
-				ajaxTabHandler.handleResponse(data, tabId);
-			},
-			beforeSend: function(jqXHR, settings) {
-				$(tabId).append("<p id=\"loader\">Carregando...</p>");
-			},
-			complete: function(jqXHR, textStatus) {
-				$("#loader", tabId).remove();
+AjaxTabHandler.prototype.handleResponseAvaliacoesFaltas = function(data, tabId) {
+	$(data).each(function(i, e) {
+		$(e).each(function(ix, ex) {
+			if (!ex[0].hasOwnProperty("data_aula")) {
+				AjaxTabHandler.prototype.handleResponseNotas(e, "avaliacoes-table");
+			} else {
+				AjaxTabHandler.prototype.handleResponseFaltas(e, "faltas-table");
 			}
 		})
 	});
+}
+
+$(document).ready(function() {
+//	$(".tab").click(function(e) {
+	var ajaxTabHandler = new AjaxTabHandler("avaliacoes_faltas-tab");
+
+	tab = $("#avaliacoes_faltas-tab");
+	tabId = $("a", tab).attr("href");
+	$.ajax({
+		url: $("#avaliacoes_faltas-tab").attr("href"),
+		dataType: "json",
+		success: function(data, textStatus, jqXHR) {
+			ajaxTabHandler.handleResponse(data, tabId);
+		},
+		beforeSend: function(jqXHR, settings) {
+			$(tabId).append("<p id=\"loader\">Carregando...</p>");
+		},
+		complete: function(jqXHR, textStatus) {
+			console.log(textStatus)
+			$("#loader", tabId).remove();
+		}
+	});
+//	});
 });
